f894285a17892528c199e095a927272d655bf7aa
Bart Kang <bart.kang.0719@gmail.com>
2025-07-08 22:26:46 -0700

    setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+      <Button variant="destructive">Delete Me!!</Button>
+    </div>
+  );
 }
