4aa79861ae381b84bf7ac32c059fb3664e5d03bb
Bart Kang <bart.kang.0719@gmail.com>
2025-08-19 13:55:37 -0700

    createcommunity modal shell (#6)

---
+++ components/icons/x-mark.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import { IconProps } from "@/types/icons";
+
+export default function XMark({ className }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={cn("size-6", className)}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
+}
