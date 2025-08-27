11c5d50d0777b9da3ba5261d60b28c6f1bc4bb78
Bart Kang <bart.kang.0719@gmail.com>
2025-08-26 19:11:51 -0700

    Community preview (#7)
    * add static preview card
    
    * add images to the preview card

---
+++ features/communities/components/community-preview.tsx
@@ -0,0 +1,59 @@
+import DefaultCommunityAvatar from "@/components/icons/default-community-avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+
+type Props = {
+  withImages: boolean;
+  banner: string;
+  avatar: string;
+};
+
+export default function CommunityPreview({
+  avatar,
+  banner,
+  withImages
+}: Props) {
+  return (
+    <Card className="pt-0 pb-4 gap-2 rounded-sm border-0 shadow-[1px_2px_4px_1px_#00000030] overflow-hidden">
+      {withImages && (
+        <div className="h-10 bg-[#fceee8]">
+          {banner && (
+            <Image
+              className="size-full object-cover"
+              src={banner}
+              alt="Community Banner"
+              width={500}
+              height={50}
+            />
+          )}
+        </div>
+      )}
+
+      <CardHeader className="px-4 pt-4 flex items-center gap-4">
+        {withImages && (
+          <Avatar className="size-12">
+            <AvatarImage
+              className="object-cover"
+              src={avatar}
+              alt="Community Avatar"
+            />
+            <AvatarFallback>
+              <DefaultCommunityAvatar className="size-full text-[#D93900]" />
+            </AvatarFallback>
+          </Avatar>
+        )}
+
+        <div>
+          <CardTitle className="text-[#181C1F] text-xl font-semibold mb-1">
+            r/communityname
+          </CardTitle>
+          <p className="text-[#5C6C74] text-xs">1 member ·1 online</p>
+        </div>
+      </CardHeader>
+      <CardContent className="px-4">
+        <p className="text-[#333D42] text-sm">Your community description</p>
+      </CardContent>
+    </Card>
+  );
+}
