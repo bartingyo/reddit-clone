12fb58f160c80ece9e3baa10b44fd17b079345b1
Bart Kang <bart.kang.0719@gmail.com>
2025-08-28 14:36:37 -0700

    implement dynamic label

--- components/ui/input.stories.tsx
+++ components/ui/input.stories.tsx
@@ -3,7 +3,10 @@ import { type Meta, type StoryObj } from "@storybook/nextjs-vite";
 
 const meta = {
   title: "Design System/Input",
-  component: Input
+  component: Input,
+  args: {
+    label: "Label"
+  }
 } satisfies Meta<typeof Input>;
 
 export default meta;
@@ -11,3 +14,8 @@ export default meta;
 type Story = StoryObj<typeof Input>;
 
 export const Default = {} satisfies Story;
+export const WithoutLabel = {
+  args: {
+    label: undefined
+  }
+} satisfies Story;
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,9 +1,16 @@
-import * as React from "react";
+"use client";
 
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
+import { ComponentProps, useId } from "react";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+type Props = {
+  label?: string;
+} & ComponentProps<"input">;
+
+function Input({ className, type, label, ...props }: Props) {
+  const labelId = useId();
+
   return (
     <Label
       className={cn(
@@ -11,23 +18,31 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
         "bg-[#E5EBEE] hover:bg-[#DBE4E9]",
         "h-14 px-4 rounded-[20px]",
         "flex items-center",
-        "focus-within:outline"
+        "focus-within:outline",
+        "cursor-text",
+        className
       )}
     >
-      <div className={cn("focus-within:translate-y-2")}>
-        <span
-          className={cn(
-            "absolute text-[#5C6C74] text-base",
-            "group-focus-within:-translate-y-4 group-focus-within:text-xs",
-            "duration-300"
-          )}
-        >
-          Label
-        </span>
+      <div
+        className={cn("relative w-full", label && "focus-within:translate-y-2")}
+      >
+        {label && (
+          <span
+            id={labelId}
+            className={cn(
+              "absolute text-[#5C6C74] text-base",
+              "group-focus-within:-translate-y-4 group-focus-within:text-xs",
+              "duration-300"
+            )}
+          >
+            {label}
+          </span>
+        )}
         <input
           type={type}
           data-slot="input"
-          className={cn("outline-none", className)}
+          className={cn("outline-none w-full")}
+          aria-labelledby={label && labelId}
           {...props}
         />
       </div>
