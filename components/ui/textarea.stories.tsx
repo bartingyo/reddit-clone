4a867087f29e9139fb0d01ae6872f37c1ff3c000
Bart Kang <bart.kang.0719@gmail.com>
2025-08-30 21:21:33 -0700

    drop component into storybook

---
+++ components/ui/textarea.stories.tsx
@@ -0,0 +1,45 @@
+import { Textarea } from "@/components/ui/textarea";
+import { type Meta, type StoryObj } from "@storybook/nextjs-vite";
+
+const meta = {
+  title: "Design System/Textarea",
+  component: Textarea,
+  args: {
+    label: "Label"
+  }
+} satisfies Meta<typeof Textarea>;
+
+export default meta;
+
+type Story = StoryObj<typeof Textarea>;
+
+export const Default = {} satisfies Story;
+export const WithoutLabel = {
+  args: {
+    label: undefined
+  }
+} satisfies Story;
+export const Required = {
+  args: {
+    required: true
+  }
+} satisfies Story;
+export const Focused = {
+  play: async ({ canvas, userEvent }) => {
+    // grab textarea element
+    const textareaElement = canvas.getByTestId("test-textarea");
+
+    // click element
+    userEvent.click(textareaElement);
+  }
+} satisfies Story;
+export const Typed = {
+  args: {
+    defaultValue: "typed"
+  }
+} satisfies Story;
+export const Error = {
+  args: {
+    "aria-invalid": true
+  }
+} satisfies Story;
--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,18 +1,71 @@
-import * as React from "react"
+"use client";
 
-import { cn } from "@/lib/utils"
+import ExclamationCircleOutline from "@/components/icons/exclamation-circle-outline";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { ComponentProps, useId } from "react";
 
-function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
+type Props = {
+  label?: string;
+} & ComponentProps<"textarea">;
+
+function Textarea({ className, label, required, ...props }: Props) {
+  const labelId = useId();
+
   return (
-    <textarea
-      data-slot="textarea"
+    <Label
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "group",
+        "bg-[#E5EBEE] hover:bg-[#DBE4E9]",
+        "h-14 px-4 rounded-[20px]",
+        "flex items-center gap-2",
+        "focus-within:outline",
+        "cursor-text",
+        "has-[textarea[aria-invalid=true]]:outline has-[textarea[aria-invalid=true]]:outline-destructive",
         className
       )}
-      {...props}
-    />
-  )
+    >
+      <div
+        className={cn(
+          "relative w-full",
+          label && "focus-within:translate-y-2",
+          "group-has-[textarea:not(:placeholder-shown)]:translate-y-2"
+        )}
+      >
+        {label && (
+          <span
+            id={labelId}
+            className={cn(
+              "absolute text-[#5C6C74] text-base",
+              "group-focus-within:-translate-y-4 group-focus-within:text-xs",
+              "group-has-[textarea:not(:placeholder-shown)]:-translate-y-4 group-has-[textarea:not(:placeholder-shown)]:text-xs",
+              "duration-300"
+            )}
+          >
+            {label}
+            {required && <span className="text-destructive ml-1">*</span>}
+          </span>
+        )}
+        <textarea
+          data-slot="textarea"
+          data-testid="test-textarea"
+          className={cn("outline-none w-full")}
+          placeholder=""
+          required={required}
+          aria-labelledby={label && labelId}
+          {...props}
+        />
+      </div>
+
+      <ExclamationCircleOutline
+        className={cn(
+          "hidden",
+          "group-has-[textarea[aria-invalid=true]]:block ",
+          "group-has-[textarea[aria-invalid=true]]:text-destructive"
+        )}
+      />
+    </Label>
+  );
 }
 
-export { Textarea }
+export { Textarea };
