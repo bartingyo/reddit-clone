aa47a8ef8bb0112a153a7e70354ec4a0d45eb3b6
Bart Kang <bart.kang.0719@gmail.com>
2025-10-05 01:35:49 -0700

    add chip selectable

--- components/ui/chip.stories.tsx
+++ components/ui/chip.stories.tsx
@@ -1,4 +1,4 @@
-import Chip from "@/components/ui/chip";
+import Chip, { ChipSelectable } from "@/components/ui/chip";
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 
 const meta = {
@@ -16,6 +16,21 @@ export const Variants = {
       <div className="flex items-center gap-2">
         <Chip>Default</Chip>
         <Chip variant={"bordered"}>Bordered</Chip>
+      </div>
+    );
+  }
+} satisfies Story;
+
+export const Selectable = {
+  render: () => {
+    return (
+      <div className="flex items-center gap-2">
+        <Chip asChild>
+          <ChipSelectable>Default</ChipSelectable>
+        </Chip>
+        <Chip asChild variant={"bordered"}>
+          <ChipSelectable>Bordered</ChipSelectable>
+        </Chip>
       </div>
     );
   }
--- components/ui/chip.tsx
+++ components/ui/chip.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
 import { cva, VariantProps } from "class-variance-authority";
 import { ComponentProps } from "react";
 
@@ -7,12 +8,19 @@ const chipVariants = cva(
     "border",
     "h-8 px-2.5 rounded-full",
     "inline-flex items-center",
-    "text-[#000000] text-xs font-semibold"
+    "text-[#000000] text-xs font-semibold",
+    "has-focus-visible:outline has-focus-visible:outline-blue-700",
+    "data-[slot=chip-selectable]:cursor-pointer",
+    "data-[slot=chip-selectable]:select-none"
   ),
   {
     variants: {
       variant: {
-        default: cn("border-transparent bg-[#E5EBEE]"),
+        default: cn(
+          "border-transparent bg-[#E5EBEE]",
+          "data-[slot=chip-selectable]:hover:bg-[#DBE4E9] data-[slot=chip-selectable]:active:bg-[#bbc2c8]",
+          "has-checked:data-[slot=chip-selectable]:bg-[#C9D7DE]"
+        ),
         bordered: cn(" border-[#00000033]")
       }
     },
@@ -22,17 +30,44 @@ const chipVariants = cva(
   }
 );
 
-type Props = VariantProps<typeof chipVariants> & ComponentProps<"span">;
+type ChipProps = VariantProps<typeof chipVariants> &
+  ComponentProps<"span"> & { asChild?: boolean };
 
 export default function Chip({
   variant,
   children,
   className,
+  asChild,
   ...props
-}: Props) {
+}: ChipProps) {
+  const Comp = asChild ? Slot : "span";
+
   return (
-    <span className={cn(chipVariants({ variant }), className)} {...props}>
+    <Comp
+      data-slot="chip"
+      className={cn(chipVariants({ variant }), className)}
+      {...props}
+    >
       {children}
-    </span>
+    </Comp>
+  );
+}
+
+type ChipSelectableProps = Pick<
+  ComponentProps<"label">,
+  "children" | "className"
+> &
+  Omit<ComponentProps<"input">, "children" | "className">;
+
+export function ChipSelectable({
+  children,
+  className,
+  ...props
+}: ChipSelectableProps) {
+  return (
+    <label data-slot="chip-selectable" className={cn("", className)}>
+      <span>{children}</span>
+      <input type="checkbox" className="sr-only" {...props} />
+    </label>
   );
 }
